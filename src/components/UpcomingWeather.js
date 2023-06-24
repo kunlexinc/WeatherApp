0e31d74d705f5dcaad153c3ea330903bfef35a44
kunlexinc <mkunle6@gmail.com>
2023-06-24 10:24:55 +0100

    Working on a weather project

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,32 +1,23 @@
-import React from "react";
-import { View, StyleSheet, StatusBar,Text } from "react-native";
+import React from 'react'
+import { View, StyleSheet, StatusBar, Text } from 'react-native'
 
-
-
-
-const UpcomingWeather = () =>{
-    return(
-        <View style={styles.container}>
-            <Text>Upcoming Weather</Text>
-        </View>
-
-
-    )
-
+const UpcomingWeather = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Upcoming Weather</Text>
+    </View>
+  )
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-     // marginTop: StatusBar.currentHeight || 0,
-      //backgroundColor: 'royalblue'
-    },
-    image: {
-      flex: 1
-    }
-  })
-  
+  container: {
+    flex: 1,
+    // marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'royalblue'
+  },
+  image: {
+    flex: 1
+  }
+})
 
-export default UpcomingWeather;
+export default UpcomingWeather
